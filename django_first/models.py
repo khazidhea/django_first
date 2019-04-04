2d517e477d423e220ac867f2b46b57e40b2f89c5
Azamat Galimzhanov <azamat@galimzhanov.com>
2019-04-04 13:17:19 +0600

    Add order process

--- django_first/models.py
+++ django_first/models.py
@@ -3,15 +3,60 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        blank=True, null=True
+    )
 
 
 class Store(models.Model):
+    location = models.CharField(max_length=100, blank=True)
+
+
+class StoreItem(models.Model):
+    store = models.ForeignKey(
+        Store, on_delete=models.CASCADE,
+        related_name='items'
+    )
     product = models.ForeignKey(
-        Product,
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        related_name='stores'
+        Product, on_delete=models.CASCADE,
+        related_name='store_items'
     )
     quantity = models.IntegerField()
+
+
+class Order(models.Model):
     location = models.CharField(max_length=100, blank=True)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        blank=True, null=True
+    )
+    is_paid = models.BooleanField(default=False)
+
+    def process(self):
+        store = Store.objects.get(location=self.location)
+        for item in self.items.all():
+            store_item = StoreItem.objects.get(
+                store=store,
+                product=item.product
+            )
+            store_item.quantity -= item.quantity
+            store_item.save()
+
+        self.price = sum(
+            (item.product.price * item.quantity for item in self.items.all())
+        )
+        self.is_paid = True
+        self.save()
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE,
+        related_name='items'
+    )
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE,
+        related_name='order_items'
+    )
+    quantity = models.IntegerField()
---
+++ django_first/tests/functional/test_order.py
@@ -0,0 +1,30 @@
+from django_first.models import Order, OrderItem, Product, Store, StoreItem
+
+
+def test_order_process(db):
+    product = Product.objects.create(
+        name='apple',
+        price=10
+    )
+    store = Store.objects.create(
+        location='Almaty',
+    )
+    store_item = StoreItem.objects.create(
+        store=store,
+        product=product,
+        quantity=100
+    )
+    order = Order.objects.create(
+        location='Almaty'
+    )
+    OrderItem.objects.create(
+        order=order,
+        product=product,
+        quantity=10
+    )
+
+    order.process()
+    store_item.refresh_from_db()
+    assert order.price == 100
+    assert order.is_paid is True
+    assert store_item.quantity == 90
