3780ae4bdb3961e74891ee19bbc49f6a052a439d
Azamat Galimzhanov <azamat@galimzhanov.com>
2019-04-11 19:26:19 +0600

    Add order list view

--- django_first/tests/functional/test_views.py
+++ django_first/tests/functional/test_views.py
@@ -1,3 +1,8 @@
+from lxml import html
+
+from django_first.models import Order
+
+
 def test_hello(db, client, data):
     client.login(username='alice', password='alice')
     response = client.get('/')
@@ -5,6 +10,9 @@ def test_hello(db, client, data):
     response = response.content.decode('utf-8')
     assert 'Hello, world!' in response
     assert 'alice' in response
+    response = html.fromstring(response)
+    orders = Order.objects.filter(customer__user__username='alice')
+    assert len(response.cssselect('li')) == orders.count()
 
 
 def test_bye(client):
--- django_first/views.py
+++ django_first/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Order
+
 
 def hello(request):
-    return render(request, 'hello.html')
+    orders = Order.objects.filter(customer__user=request.user)
+    return render(request, 'hello.html', context={
+        'orders': orders
+    })
 
 
 def bye(request):
