98d95944d647428453821ff3103957416d571fe3
Azamat Galimzhanov <azamat@galimzhanov.com>
2019-06-13 20:11:34 +0600

    Add image field

--- django_first/urls.py
+++ django_first/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -28,4 +30,4 @@ urlpatterns = [
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
     path('bye/', views.ByeView.as_view()),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
